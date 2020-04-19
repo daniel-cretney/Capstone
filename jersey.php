a7ef61e0419646148cb55f5cb06e94e1447cc794
daniel-cretney <61253525+daniel-cretney@users.noreply.github.com>
2020-04-19 15:07:25 -0400

    Add files via upload

--- jersey.php
+++ jersey.php
@@ -9,32 +9,29 @@
 	<title>Dealer Dan's</title>
 	<link rel='stylesheet' type='text/css' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'>
 	<link rel='stylesheet' type='text/css' href='jerseys.css'>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 	<link rel='stylesheet' type='text/css' href='page_format.css'>
 	<script src="https://kit.fontawesome.com/f46d11bbae.js" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-	
 </head>
 <body>
 <nav class='navbar navbar-expand-md navbar-light bg-light sticky-top'>
 <div class='container-fluid'>
-	<a class='navbar-brand' href='index.html'><img class='brand'src='img/dealerdans.png'>   Dealer Dan's</a>
-	<button class='navbar-toggler' type='button' data-toggle='collapse'
-	data-target='#navbarResponsive'>
+	<a class='navbar-brand' href='index.html'><img class='brand'src='img/dealerdans.png'>  Dealer Dan's</a>
+	<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive'>
 		<span class='navbar-toggler-icon'></span>
 	</button>
 	<div class='collapse navbar-collapse' id='navbarResponsive'>
 		<ul class='navbar-nav ml-auto'>
 			<li class='nav-item'>
-				<a class='nav-link' href='index.html'>Home</a>
+				<a class='nav-link' href='index.php'>Home</a>
 			</li>
 			<li class='nav-item'>
-				<a class='nav-link active' href='jerseys.html'>Jerseys</a>
+				<a class='nav-link active' href='jersey.php'>Jerseys</a>
 			</li>
 			<li class='nav-item'>
-				<a class='nav-link' href='login.html'>Sign In</a>
+				<a class='nav-link' href='login.php'>Sign In</a>
 			<li class='nav-item'>
 				<a class='nav-link' href='cart.php'>Cart</a>
 			</li>
@@ -44,51 +41,52 @@
 </nav>
 <div class='container'>
 	<form action='cart.php' method='POST'>
-<?php
-	$stmt = $pdo->query('SELECT * FROM Inventory');
-	$count = 0;
-	while ($row = $stmt->fetch()){
-		if ($count = 0) {
-			?>
-			<div class='row'>
-				<div class='col-md-4'>
-					<img src="<?php echo $row['url'];?>" class="d-block w-100" name='Name' alt="Name">
-					<h3><?php echo $row['Name']; ?></h3>
-					<h5>Size: <?php echo $row['Size']; ?></h5>
-					<h5>Price: <?php echo $row['Price']; ?></h5>
-					<button type='submit' name='add to cart' class='btn-btn-default cart'>Add to Cart</button>
+		<?php
+			$stmt = $pdo->query('SELECT * FROM Inventory');
+			$count = 0;
+			while ($row = $stmt->fetch()){
+				if ($count == 0){
+					?>
+					<div class='row'>
+					<div class='col-md-4'>
+						<img src="<?php echo $row['url'];?>" class="d-block w-100" name='Name' alt="Name">
+						<h3><?php echo $row['Name']; ?></h3>
+						<h5>Size: <?php echo $row['Size']; ?></h5>
+						<h5>Price: <?php echo $row['Price']; ?></h5>
+						<button type='submit' name='add to cart' class='btn-btn-default cart'>Add to Cart</button>
+					</div>
+					<?php
+					$count ++;}
+					elseif ($count % 3 == 0){
+					?>
+					</div>
+					<div class='row'>
+					<div class='col-md-4'>
+						<img src="<?php echo $row['url'];?>" class="d-block w-100" name='Name' alt="Name">
+						<h3><?php echo $row['Name']; ?></h3>
+						<h5>Size: <?php echo $row['Size']; ?></h5>
+						<h5>Price: <?php echo $row['Price']; ?></h5>
+						<button type='submit' name='add to cart' class='btn-btn-default cart'>Add to Cart</button>
+					</div>
+					<?php
+					$count ++;}
+					else{
+					?>
+					<div class='col-md-4'>
+						<img src="<?php echo $row['url'];?>" class="d-block w-100" name='Name' alt="Name">
+						<h3><?php echo $row['Name']; ?></h3>
+						<h5>Size: <?php echo $row['Size']; ?></h5>
+						<h5>Price: <?php echo $row['Price']; ?></h5>
+						<button type='submit' name='add to cart' class='btn-btn-default cart'>Add to Cart</button>
+					</div>
+					<?php
+					$count ++;}
+					}
+					?>
+	</form>
 				</div>
-	<?php 
-		$count ++; 
-		}elseif ($count % 3 == 0) {
-			?>
-			</div>
-			<div class='row'>
-				<div class='col-md-4'>
-					<img src="<?php echo $row['url'];?>" class="d-block w-100" name='Name' alt="Name">
-					<h3><?php echo $row['Name']; ?></h3>
-					<h5>Size: <?php echo $row['Size']; ?></h5>
-					<h5>Price: <?php echo $row['Price']; ?></h5>
-					<button type='submit' name='add to cart' class='btn-btn-default cart'>Add to Cart</button>
-				</div>
-	<?php
-		$count ++; }
-		else{
-			?>
-			<div class='col-md-4'>
-				<img src="<?php echo $row['url'];?>" class="d-block w-100" name='Name' alt="Name">
-				<h3><?php echo $row['Name']; ?></h3>
-				<h5>Size: <?php echo $row['Size']; ?></h5>
-				<h5>Price: <?php echo $row['Price']; ?></h5>
-				<button type='submit' name='add to cart' class='btn-btn-default cart'>Add to Cart</button>
-			</div>
-	<?php
-		$count ++;}
-	}
-	?>
 </div>
-</form>
-</div>
+
 <footer>
 <div class='container-fluid padding'>
 <div class='row text-center'>
